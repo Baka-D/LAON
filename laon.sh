885e7fcba9ce41603f323692063a0e7306a311c0
Qiran Wang <abcde11819@live.com>
2017-01-17 19:31:58 +0800

    Update laon.sh

--- laon.sh
+++ laon.sh
@@ -80,8 +80,9 @@ pre_install(){
 
 # Making File-Cache Dir
 make_dir(){
-    mkdir /tmp/LAON
-    cd /tmp/LAON
+    mkdir /opt/LAON
+    mkdir /opt/LAON/tmp
+    cd /opt/LAON/tmp
 }
 
 # Install Apr
@@ -94,7 +95,7 @@ install_apr(){
     mv apr-1.5.2 apr
     cd apr
     ./buildconf
-    ./configure --prefix=/opt/apr
+    ./configure --prefix=/opt/LAON/apr
     make && make install
 }
 
@@ -107,7 +108,7 @@ install_apr_util(){
     mv apr-util-1.5.4 apr-util
     cd apr-util
     ./buildconf
-    ./configure --prefix=/opt/apr-util --with-apr=/opt/apr
+    ./configure --prefix=/opt/LAON/apr-util --with-apr=/opt/LAON/apr
     make && make install
 }
 
@@ -129,7 +130,7 @@ install_pcre(){
     tar -zxf pcre-8.38.tar.gz
     rm pcre-8.38.tar.gz
     cd pcre-8.38
-    ./configure --prefix=/opt/pcre
+    ./configure --prefix=/opt/LAON/pcre
     make && make install
 }
 
@@ -140,7 +141,7 @@ install_nghttp2(){
     tar -zxf nghttp2*.tar.gz
     rm nghttp2*.tar.gz
     cd nghttp2*
-    ./configure --prefix=/opt/nghttp2
+    ./configure --prefix=/opt/LAON/nghttp2
     make && make install
 }
 
@@ -152,7 +153,7 @@ install_openssl(){
     rm OpenSSL*.tar.gz
     mv *openssl* openssl
     cd openssl
-    ./config --prefix=/opt/openssl enable-shared
+    ./config --prefix=/opt/LAON/openssl enable-shared
     make && make install
 }
 
@@ -166,7 +167,7 @@ install_apache(){
     ln -s /tmp/LAON/apr srclib/apr
     ln -s /tmp/LAON/apr-util srclib/apr-util
     ./buildconf
-    ./configure --prefix=/opt/httpd --enable-deflate --enable-expires --enable-headers --enable-modules=all --enable-so --enable-mpm --with-mpm=prefork --enable-rewrite --with-apr=/opt/apr --with-apr-util=/opt/apr-util --with-pcre=/opt/pcre/bin/pcre-config --enable-ssl --enable-rewrite --enable-http2 --with-nghttp2=/opt/nghttp2 --with-ssl=/opt/openssl --with-crypto
+    ./configure --prefix=/opt/LAON/httpd --enable-deflate --enable-expires --enable-headers --enable-modules=all --enable-so --enable-mpm --with-mpm=prefork --enable-rewrite --with-apr=/opt/LAON/apr --with-apr-util=/opt/LAON/apr-util --with-pcre=/opt/LAON/pcre/bin/pcre-config --enable-ssl --enable-rewrite --enable-http2 --with-nghttp2=/opt/LAON/nghttp2 --with-ssl=/opt/LAON/openssl --with-crypto
     make && make install
 }
 
@@ -187,16 +188,16 @@ config(){
     echo "Configing"
     #chown www-root:www-root /var/www/blog/public -R
     #chmod 755 /var/www/blog/public -R
-    cp /opt/openssl/lib/* /usr/lib/ -R -f
-    cp /opt/httpd/bin/* /usr/bin/ -R -f
-    cp /opt/nghttp2/lib/* /usr/lib/ -R -f
+    cp /opt/LAON/openssl/lib/* /usr/lib/ -R -f
+    cp /opt/LAON/httpd/bin/* /usr/bin/ -R -f
+    cp /opt/LAON/nghttp2/lib/* /usr/lib/ -R -f
     
 }
 
 # Delete Downloaded File
 delete_files(){
     echo "Deleting useless files"
-    rm -rf /tmp/LAON
+    rm -rf /opt/LAON/tmp
     echo "Install Complete!"
 }
 
@@ -220,12 +221,12 @@ install_LAON(){
 
 # Uninstall LAON
 uninstall_LAON(){
-    rm -rf /opt/httpd /opt/openssl /opt/nghttp2 /opt/apr /opt/apr-util /opt/pcre
+    rm -rf /opt/LAON
 	#cd ${hexopatch}
 	#node uninstall
 	#rm -rf ${hexopatch}
 	#rm -rf /usr/local/n
-	rm /usr/bin/node
+	#rm /usr/bin/node
 	rm /usr/bin/httpd
 	rm /usr/bin/apachectl
 	#apt-get remove npm -y
